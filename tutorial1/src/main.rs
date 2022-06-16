7584eef7077d0531413ad7b8414910e3d4f73d46
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-06-15 23:04:41 -0500

    constants, variables and shadowing

--- tutorial1/src/main.rs
+++ tutorial1/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // Mutable variables, let mut x = 4;
+    let x = 4;
+    println!("x is : {}", x);
+    let x = x + 1;
+    println!("x is : {}", x);
+
+    // Shadowing
+    {
+        let x =  x - 2;
+        println!("Interior Scope variable");
+        println!("x is: {}", x);
+    }
+    let x = x + 1;
+    println!("x is : {}", x);
+
+    let x = "hello";
+    println!("x is: {}", x);
+
+    // Constants
+    const SECOND_IN_MINUTE: u32 = 60;
+    const SECOND_IN_MINUTE: u32 = 100;
+    println!("{}", SECOND_IN_MINUTE);
 }
